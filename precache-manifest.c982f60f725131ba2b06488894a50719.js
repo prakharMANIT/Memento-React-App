360843cb3d5dc224c88237b7a2f5478b47ec38dd
prakharMANIT <101503529+prakharMANIT@users.noreply.github.com>
2022-06-25 14:33:23 +0530

    Updates

---
+++ precache-manifest.c982f60f725131ba2b06488894a50719.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2247a3d5d71bab7dba8003ec65c3f1",
+    "url": "/Memento-React-App/index.html"
+  },
+  {
+    "revision": "de8f65762da05508a3b1",
+    "url": "/Memento-React-App/static/css/main.fa5a09dd.chunk.css"
+  },
+  {
+    "revision": "2fe0e55cd1c3dadf8a3f",
+    "url": "/Memento-React-App/static/js/2.b4b65a04.chunk.js"
+  },
+  {
+    "revision": "cd94619111e2ab78ce54eac27b302772",
+    "url": "/Memento-React-App/static/js/2.b4b65a04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de8f65762da05508a3b1",
+    "url": "/Memento-React-App/static/js/main.ba1c3ca1.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Memento-React-App/static/js/main.ba1c3ca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "553f59c9cc2b813c8d18",
+    "url": "/Memento-React-App/static/js/runtime-main.bc5194ff.js"
+  },
+  {
+    "revision": "56f88b03e55e9b2cefb5fb9be8ec54b5",
+    "url": "/Memento-React-App/static/media/memories.56f88b03.png"
+  }
+]);
